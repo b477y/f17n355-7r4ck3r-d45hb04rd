84b5a4ea1303b242e9d79b0fd76684373fd36765
b477y <mohammedsaleh002@outlook.com>
2024-10-15 17:39:50 +0300

    Update userModel.js schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,63 +1,49 @@
-
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
-    {
-      name: {
-          type: String, 
-          trim: true,
-          required: [true, 'name required']
-      },
-  
-      email: {
-          type: String, 
-          required: [true, 'email required'],
-          unique: true,
-          lowercase: true
-      },
-  
-      age: Number,
-      weight: Number,
-      height: Number,
-      waterGoal: Number,
-      waterDrank: {
-        type: Number,
-        default: 0
-      },
-      caloriesToBurn: Number,
-  
-  
-      password: {
-          type: String,
-          required: [true, 'password required'],
-          minLength : [6, 'too short password']
-      },
-      passwordChangedAt: Date,
-      passwordResetCode: String,
-      passwordResetExpires: String,
-      passwordResetVerified: Boolean
-      ,
-     
-  
-      active: {
-          type: Boolean,
-          default: true
-      }
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "name required"],
     },
-    { timestamps: true }
-  );
-  
-  userSchema.pre('save', async function (next) {
-      if (!this.isModified('password')) return next();
-      
-      this.password = await bcrypt.hash(this.password, 12);
-      next();
-  })
-  // compare password method
-  userSchema.methods.comparePassword = async function (enteredPassword) {
-      return await bcrypt.compare(enteredPassword, this.password);
-  }
-  const User =mongoose.model("User", userSchema);
+
+    email: {
+      type: String,
+      required: [true, "email required"],
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "password required"],
+      minLength: [6, "too short password"],
+    },
+
+    passwordChangedAt: Date,
+    passwordResetCode: String,
+    passwordResetExpires: String,
+    passwordResetVerified: Boolean,
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+// compare password method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+const User = mongoose.model("User", userSchema);
 
 export default User;
